009a598a5e75049f2ae2c2ce75b713c29366b085
WojciechMat <wmatsad@gmail.com>
2024-04-28 23:36:26 +0200

    better masks

--- data/masked_midi_dataset.py
+++ data/masked_midi_dataset.py
@@ -1,10 +1,17 @@
 # For the future
 special_tokens = [
     "<CLS>",
-    "<EOS>" "<MASK_RANDOM>",
-    "<MASK_HIGH>",
-    "<MASK_LOW>",
-    "<MASK_MID>",
-    "<MASK_FORTE>",
-    "<MASK_PIANO>",
+    "<EOS>",
+    "<MASK_RANDOM>",
+    "<MASK_PPP>",
+    "<MASK_PP>",
+    "<MASK_P>",
+    "<MASK_MP>",
+    "<MASK_MF>",
+    "<MASK_F>",
+    "<MASK_BASS>",
+    "<MASK_TENOR>",
+    "<MASK_ALTO>",
+    "<MASK_SOPRANO>",
+    "<MASK_TREBLE>",
 ]
