1e86d35d2101fe4dfab1a41224414437e9cd83c9
WojciechMat <wmatsad@gmail.com>
2024-04-29 11:00:02 +0200

    requirements, update training_plan

--- training_plan.sh
+++ training_plan.sh
@@ -1,4 +1,5 @@
 # Run this first and wait for the datasets to cache
+python3.10 -m scripts.train_awesome_tokenizer
 python3.10 -m scripts.cache_all_datasets
 
 # train on exponential datasets
@@ -14,7 +15,6 @@ python3.10 -m gpt2.train --config-name=gpt2_small data = "awesome-giant"
 # train without augmentation
 python3.10 -m gpt2.train --config-name=gpt2_pretraining model="gpt2" data="awesome-giant" data.dataset_name: "giant-mid-coarse"
 python3.10 -m gpt2.train --config-name=gpt2_pretraining model="gpt2" data="exponential-giant" data.dataset_name: "giant-mid-coarse"
-
 
 # Try training a large model
 python3.10 -m gpt2.train --config-name=gpt2_pretraining model="gpt2_large" data="awesome-colossal"
