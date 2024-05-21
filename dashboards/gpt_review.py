31f903007753894214351e3b0a284e12a57d6e87
WojciechMat <wmatsad@gmail.com>
2024-05-21 21:55:32 +0200

    generate midi from file script

--- dashboards/gpt_review.py
+++ dashboards/gpt_review.py
@@ -1,6 +1,7 @@
 import os
 import json
 from glob import glob
+from typing import Tuple
 from contextlib import nullcontext
 
 import yaml
@@ -19,27 +20,25 @@ from dashboards.common.components import download_button
 from tokenized_midi_datasets import OneTimeTokenDataset, AwesomeTokensDataset, ExponentialTimeTokenDataset
 
 
-def select_part_dataset(midi_dataset: Dataset):
+def select_part_dataset(midi_dataset: Dataset) -> Dataset:
+    """
+    Allows the user to select a part of the dataset based on composer and title.
+    """
     source_df = midi_dataset.to_pandas()
     source_df["source"] = source_df["source"].map(lambda source: yaml.safe_load(source))
     source_df["composer"] = [source["composer"] for source in source_df.source]
     source_df["title"] = [source["title"] for source in source_df.source]
 
     composers = source_df.composer.unique()
-
     selected_composer = st.selectbox(
-        label="Select composer",
+        "Select composer",
         options=composers,
         index=3,
     )
 
     ids = source_df.composer == selected_composer
     piece_titles = source_df[ids].title.unique()
-    
-    selected_title = st.selectbox(
-        label="Select title",
-        options=piece_titles,
-    )
+    selected_title = st.selectbox("Select title", options=piece_titles)
 
     ids = (source_df.composer == selected_composer) & (source_df.title == selected_title)
     part_df = source_df[ids]
@@ -48,114 +47,78 @@ def select_part_dataset(midi_dataset: Dataset):
     return part_dataset
 
 
-def main():
-    with st.sidebar:
-        devices = [f"cuda:{it}" for it in range(torch.cuda.device_count())] + ["cpu"]
-        device = st.selectbox(label="device", options=devices)
-        checkpoints = glob("checkpoints/*/*.pt")
-        checkpoint_path = st.selectbox(label="checkpoint", options=checkpoints)
+def load_dataset_name_and_tokenizer(
+    checkpoint: str,
+    device: torch.device,
+) -> Tuple[OmegaConf, dict, object]:
+    """
+    Loads the model configuration, dataset configuration, and tokenizer based on the checkpoint path.
+    """
+    train_config = checkpoint["config"]
+    cfg = OmegaConf.create(train_config)
 
-        torch.manual_seed(4)
-        torch.cuda.manual_seed(4)
-        torch.backends.cuda.matmul.allow_tf32 = True  # allow tf32 on matmul
-        torch.backends.cudnn.allow_tf32 = True  # allow tf32 on cudnn
-        device_type = "cuda" if "cuda" in device else "cpu"  # for later use in torch.autocast
-
-        checkpoint = torch.load(f=checkpoint_path, map_location=device)
-
-        train_config = checkpoint["config"]
-        cfg = OmegaConf.create(train_config)
-        ptdtype = {"float32": torch.float32, "bfloat16": torch.bfloat16, "float16": torch.float16}[cfg.system.dtype]
-        ctx = nullcontext() if device_type == "cpu" else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
-    base_dataset_path = st.text_input(label="base dataset", value="roszcz/maestro-sustain-v2")
-    dataset_split = st.selectbox(label="split", options=["validation", "train", "test"])
-
-    # This if is for compatibility with models trained before 1.05
-    if "dataset_name" in train_config["data"].keys():
+    if "dataset_name" in train_config["data"]:
         config_name = cfg.data.dataset_name
         if cfg.data.tokenizer == "OneTimeTokenizer":
             dataset_name = "OneTimeTokenDataset"
             dataset_config = OneTimeTokenDataset.builder_configs[config_name].builder_parameters
-            dataset_config = OmegaConf.create(dataset_config)
-
-            tokenizer = OneTimeTokenizer(**dataset_config.tokenizer_parameters)
-
+            tokenizer = OneTimeTokenizer(**dataset_config["tokenizer_parameters"])
         elif cfg.data.tokenizer == "NoLossTokenizer":
             dataset_name = "ExponentialTimeTokenDataset"
             dataset_config = ExponentialTimeTokenDataset.builder_configs[config_name].builder_parameters
-            dataset_config = OmegaConf.create(dataset_config)
-
-            tokenizer = NoLossTokenizer(**dataset_config.tokenizer_parameters)
-
+            tokenizer = NoLossTokenizer(**dataset_config["tokenizer_parameters"])
         elif cfg.data.tokenizer == "AwesomeMidiTokenizer":
             dataset_name = "AwesomeTokensDataset"
             dataset_config = AwesomeTokensDataset.builder_configs[config_name].builder_parameters
-            dataset_config = OmegaConf.create(dataset_config)
-
-            min_time_unit = dataset_config.tokenizer_parameters["min_time_unit"]
-            n_velocity_bins = dataset_config.tokenizer_parameters["n_velocity_bins"]
+            min_time_unit = dataset_config["tokenizer_parameters"]["min_time_unit"]
+            n_velocity_bins = dataset_config["tokenizer_parameters"]["n_velocity_bins"]
             tokenizer_path = f"pretrained/awesome_tokenizers/awesome-tokenizer-{min_time_unit}-{n_velocity_bins}.json"
             tokenizer = AwesomeMidiTokenizer.from_file(tokenizer_path)
-
     else:
         dataset_config = cfg.dataset
-
         if cfg.data.tokenizer == "OneTimeTokenizer":
             dataset_name = "OneTimeTokenDataset"
-            tokenizer = OneTimeTokenizer(**dataset_config.tokenizer_parameters)
-
+            tokenizer = OneTimeTokenizer(**dataset_config["tokenizer_parameters"])
         elif cfg.data.tokenizer == "NoLossTokenizer":
             dataset_name = "ExponentialTimeTokenDataset"
-            tokenizer = NoLossTokenizer(**dataset_config.tokenizer_parameters)
-
+            tokenizer = NoLossTokenizer(**dataset_config["tokenizer_parameters"])
         elif cfg.data.tokenizer == "AwesomeMidiTokenizer":
-            tokenizer_path = "pretrained/awesome_tokenizers/awesome-tokenizer-pretrained.json"
-            min_time_unit = dataset_config.tokenizer_parameters["min_time_unit"]
-            n_velocity_bins = dataset_config.tokenizer_parameters["n_velocity_bins"]
+            dataset_name = "AwesomeTokensDataset"
+            min_time_unit = dataset_config["tokenizer_parameters"]["min_time_unit"]
+            n_velocity_bins = dataset_config["tokenizer_parameters"]["n_velocity_bins"]
             tokenizer_path = f"pretrained/awesome_tokenizers/awesome-tokenizer-{min_time_unit}-{n_velocity_bins}.json"
             tokenizer = AwesomeMidiTokenizer.from_file(tokenizer_path)
-            dataset_name = "AwesomeTokensDataset"
 
-    dataset_config.base_dataset_name = base_dataset_path
-    # Do not use any extra datasets
-    dataset_config.extra_datasets = []
-    # Disable augmentation
-    dataset_config.augmentation_repetitions = 0
+    return cfg, dataset_config, dataset_name, tokenizer
 
-    dataset = load_dataset(
-        f"tokenized_midi_datasets/{dataset_name}",
-        split=dataset_split,
-        trust_remote_code=True,
-        num_proc=8,
-        **dataset_config,
-    )
-    dataset = select_part_dataset(midi_dataset=dataset)
 
-    # model init
-    model_args = dict(
-        n_layer=cfg.model.n_layer,
-        n_head=cfg.model.n_head,
-        n_embd=cfg.model.n_embd,
-        block_size=dataset_config.sequence_length,
-        bias=cfg.model.bias,
-        vocab_size=None,
-        dropout=cfg.model.dropout,
-    )  # start with model_args from command line
+def initialize_model(
+    cfg: OmegaConf,
+    dataset_config: dict,
+    checkpoint: dict,
+    device: torch.device,
+) -> GPT:
+    """
+    Initializes the GPT model using the given configurations and checkpoint.
+    """
+    model_args = {
+        "n_layer": cfg.model.n_layer,
+        "n_head": cfg.model.n_head,
+        "n_embd": cfg.model.n_embd,
+        "block_size": dataset_config["sequence_length"],
+        "bias": cfg.model.bias,
+        "vocab_size": None,
+        "dropout": cfg.model.dropout,
+    }
 
     checkpoint_model_args = checkpoint["model_args"]
-
-    # force these config attributes to be equal otherwise we can't even resume training
-    # the rest of the attributes (e.g. dropout) can stay as desired from command line
     for k in ["n_layer", "n_head", "n_embd", "block_size", "bias", "vocab_size"]:
         model_args[k] = checkpoint_model_args[k]
 
-    # create the model
     gptconf = GPTConfig(**model_args)
     model = GPT(gptconf)
     state_dict = checkpoint["model"]
 
-    # fix the keys of the state dictionary :(
-    # honestly no idea how checkpoints sometimes get this prefix, have to debug more
     unwanted_prefix = "_orig_mod."
     for k, v in list(state_dict.items()):
         if k.startswith(unwanted_prefix):
@@ -165,131 +128,131 @@ def main():
     model.eval()
     model.to(device)
 
-    with st.expander("train config"):
-        st.json(train_config)
+    return model
 
-    with st.expander("dataset config"):
-        st.write(dataset_config)
 
-    idx = st.number_input(label="record_id", value=0, max_value=len(dataset))
+def main():
+    with st.sidebar:
+        # Select device and checkpoint path
+        devices = [f"cuda:{it}" for it in range(torch.cuda.device_count())] + ["cpu"]
+        device = st.selectbox("device", options=devices)
+        checkpoint_path = st.selectbox("checkpoint", options=glob("checkpoints/*/*.pt"))
+
+        torch.manual_seed(4)
+        torch.cuda.manual_seed(4)
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
+        device_type = "cuda" if "cuda" in device else "cpu"
+        ctx = (
+            nullcontext()
+            if device_type == "cpu"
+            else torch.amp.autocast(
+                device_type=device_type,
+                dtype=torch.float32,
+            )
+        )
+
+        # Load model, tokenizer, and configurations
+        checkpoint = torch.load(checkpoint_path, map_location=device)
+        cfg, dataset_config, dataset_name, tokenizer = load_dataset_name_and_tokenizer(checkpoint, device)
+        model = initialize_model(cfg, dataset_config, checkpoint=checkpoint, device=device)
+
+    base_dataset_path = st.text_input("base dataset", value="roszcz/maestro-sustain-v2")
+    dataset_split = st.selectbox("split", options=["validation", "train", "test"])
+
+    dataset_config["base_dataset_name"] = base_dataset_path
+    dataset_config["extra_datasets"] = []
+    dataset_config["augmentation_repetitions"] = 0
+
+    dataset = load_dataset(
+        f"tokenized_midi_datasets/{dataset_name}",
+        split=dataset_split,
+        trust_remote_code=True,
+        num_proc=8,
+        **dataset_config,
+    )
+
+    # Select part of the dataset
+    dataset = select_part_dataset(midi_dataset=dataset)
+
+    # Get the record id from user input
+    idx = st.number_input("record_id", value=0, max_value=len(dataset))
     record = dataset[idx]
     source = json.loads(record["source"])
 
-    with st.expander(label="source"):
-        st.json(source)
-
+    # Decode and display the original piece
     notes = tokenizer.decode(record["note_token_ids"])
     piece = ff.MidiPiece(notes, source=source)
-    st.write(
-        """
-        If the tokenizer sees unmatched NOTE_ON events
-        it will treat them as if the note was pressed until the end of the recording.
-        If the tokenizef sees umatched NOTE_OFF rents, it will ignore it.
-        If it encounters a NaN or end <= start, the note is invalid.
-        """
-    )
+
     with st.form("generate parameters"):
-        temperature = st.number_input(label="temperature", value=1.0)
-        max_new_tokens = st.number_input(label="max_new_tokens", value=dataset_config.sequence_length)
+        temperature = st.number_input("temperature", value=1.0)
+        max_new_tokens = st.number_input("max_new_tokens", value=dataset_config["sequence_length"])
         run = st.form_submit_button("Generate")
+
     if not run:
         return
-    
+
+    # Generate new tokens and create the generated piece
     input_sequence = torch.tensor([record["note_token_ids"]], device=device)
     with torch.no_grad():
         with ctx:
-            output = model.generate(
-                idx=input_sequence,
-                max_new_tokens=max_new_tokens,
-                temperature=temperature,
-            )
+            output = model.generate(input_sequence, max_new_tokens=max_new_tokens, temperature=temperature)
 
-    output: torch.Tensor = output[0].cpu().numpy()  # , dataset_config.sequence_length :]
-    # we want to decode the whole output so that the pressed notes can be unpressed by the tokenizer
+    output = output[0].cpu().numpy()
     out_notes = tokenizer.decode(output)
     out_piece = ff.MidiPiece(out_notes)
-    # start from new model-generated notes
     generated_notes = out_notes.iloc[piece.size :].copy()
-    generated_piece = ff.MidiPiece(df=generated_notes)
+    generated_piece = ff.MidiPiece(generated_notes)
     generated_piece.time_shift(-generated_piece.df.start.min())
-    io_columns = st.columns(2)
 
-    title = source["title"]
-    composer = source["composer"]
+    io_columns = st.columns(2)
+    title, composer = source["title"], source["composer"]
     piece_name = (title + composer).replace(" ", "_").casefold()
 
+    # Display and allow download of the original MIDI
     with io_columns[0]:
         st.write("original:")
         streamlit_pianoroll.from_fortepyan(piece=piece)
-
-        with st.expander(label="tokens"):
-            st.write([tokenizer.vocab[idx] for idx in record["note_token_ids"]])
-
         original_midi_path = f"tmp/fragment_of_{piece_name}_{idx}.mid"
         source_file = piece.to_midi()
+        source_file.write(original_midi_path)
+        with open(original_midi_path, "rb") as file:
+            st.markdown(
+                download_button(file.read(), original_midi_path.split("/")[-1], "Download source midi"),
+                unsafe_allow_html=True,
+            )
+        os.unlink(original_midi_path)
 
-        try:
-            source_file.write(original_midi_path)
-            with open(original_midi_path, "rb") as file:
-                download_button_str = download_button(
-                    object_to_download=file.read(),
-                    download_filename=original_midi_path.split("/")[-1],
-                    button_text="Download source midi",
-                )
-                st.markdown(download_button_str, unsafe_allow_html=True)
-        finally:
-            # make sure to always clean up
-            os.unlink(original_midi_path)
-
+    # Display and allow download of the generated MIDI
     with io_columns[1]:
         st.write("generated:")
-
         streamlit_pianoroll.from_fortepyan(piece=generated_piece)
-
         output = output[input_sequence.shape[-1] :]
-        with st.expander("generated tokens"):
-            st.write([tokenizer.vocab[idx] for idx in output])
-
         milion_parameters = model.get_num_params() / 1e6
         midi_path = f"tmp/{milion_parameters:.0f}_variations_on_{piece_name}_{idx}.mid"
         generated_file = generated_piece.to_midi()
-
-        try:
-            generated_file.write(midi_path)
-            with open(midi_path, "rb") as file:
-                download_button_str = download_button(
-                    object_to_download=file.read(),
-                    download_filename=midi_path.split("/")[-1],
-                    button_text="Download generated midi",
-                )
-                st.markdown(download_button_str, unsafe_allow_html=True)
-        finally:
-            # make sure to always clean up
-            os.unlink(midi_path)
+        generated_file.write(midi_path)
+        with open(midi_path, "rb") as file:
+            st.markdown(
+                download_button(file.read(), midi_path.split("/")[-1], "Download generated midi"),
+                unsafe_allow_html=True,
+            )
+        os.unlink(midi_path)
 
     st.write("whole model output")
-    generated_notes_with_offset = out_notes[piece.size :].copy()
-    second_part = ff.MidiPiece(generated_notes_with_offset)
-
-    # Model could have also add "NOTE_OFF" events to original sequence
-    expanded_input_notes = out_notes[: piece.size].copy()
-    expanded_piece = ff.MidiPiece(expanded_input_notes)
+    second_part = ff.MidiPiece(out_notes[piece.size :].copy())
+    expanded_piece = ff.MidiPiece(out_notes[: piece.size].copy())
     streamlit_pianoroll.from_fortepyan(piece=expanded_piece, secondary_piece=second_part)
 
+    # Allow download of the full MIDI with context
     full_midi_path = f"tmp/full_{milion_parameters}_variations_on_{piece_name}_{idx}.mid"
-    out_file = out_piece.to_midi()
-    try:
-        out_file.write(full_midi_path)
-        with open(full_midi_path, "rb") as file:
-            download_button_str = download_button(
-                object_to_download=file.read(),
-                download_filename=full_midi_path.split("/")[-1],
-                button_text="Download midi with context",
-            )
-            st.markdown(download_button_str, unsafe_allow_html=True)
-    finally:
-        # make sure to always clean up
-        os.unlink(full_midi_path)
+    out_piece.to_midi().write(full_midi_path)
+    with open(full_midi_path, "rb") as file:
+        st.markdown(
+            download_button(file.read(), full_midi_path.split("/")[-1], "Download midi with context"),
+            unsafe_allow_html=True,
+        )
+    os.unlink(full_midi_path)
 
 
 if __name__ == "__main__":
---
+++ scripts/generate_from_midi.py
@@ -0,0 +1,161 @@
+import argparse
+from contextlib import nullcontext
+
+import torch
+import pandas as pd
+import fortepyan as ff
+from omegaconf import OmegaConf, DictConfig
+from midi_trainable_tokenizers import AwesomeMidiTokenizer
+from midi_tokenizers import MidiTokenizer, NoLossTokenizer, OneTimeTokenizer
+
+from gpt2.model import GPT, GPTConfig
+from tokenized_midi_datasets import OneTimeTokenDataset, AwesomeTokensDataset, ExponentialTimeTokenDataset
+
+
+def load_model_and_tokenizer(checkpoint_path: str, device: torch.device) -> tuple[GPT, MidiTokenizer, DictConfig]:
+    checkpoint = torch.load(f=checkpoint_path, map_location=device)
+    train_config = checkpoint["config"]
+    cfg = OmegaConf.create(train_config)
+
+    if "dataset_name" in train_config["data"].keys():
+        config_name = cfg.data.dataset_name
+        if cfg.data.tokenizer == "OneTimeTokenizer":
+            dataset_config = OneTimeTokenDataset.builder_configs[config_name].builder_parameters
+            tokenizer = OneTimeTokenizer(**dataset_config["tokenizer_parameters"])
+        elif cfg.data.tokenizer == "NoLossTokenizer":
+            dataset_config = ExponentialTimeTokenDataset.builder_configs[config_name].builder_parameters
+            tokenizer = NoLossTokenizer(**dataset_config["tokenizer_parameters"])
+        elif cfg.data.tokenizer == "AwesomeMidiTokenizer":
+            dataset_config = AwesomeTokensDataset.builder_configs[config_name].builder_parameters
+            min_time_unit = dataset_config["tokenizer_parameters"]["min_time_unit"]
+            n_velocity_bins = dataset_config["tokenizer_parameters"]["n_velocity_bins"]
+            tokenizer_path = f"pretrained/awesome_tokenizers/awesome-tokenizer-{min_time_unit}-{n_velocity_bins}.json"
+            tokenizer = AwesomeMidiTokenizer.from_file(tokenizer_path)
+    else:
+        dataset_config = cfg.dataset
+        if cfg.data.tokenizer == "OneTimeTokenizer":
+            tokenizer = OneTimeTokenizer(**dataset_config["tokenizer_parameters"])
+        elif cfg.data.tokenizer == "NoLossTokenizer":
+            tokenizer = NoLossTokenizer(**dataset_config["tokenizer_parameters"])
+        elif cfg.data.tokenizer == "AwesomeMidiTokenizer":
+            min_time_unit = dataset_config["tokenizer_parameters"]["min_time_unit"]
+            n_velocity_bins = dataset_config["tokenizer_parameters"]["n_velocity_bins"]
+            tokenizer_path = f"pretrained/awesome_tokenizers/awesome-tokenizer-{min_time_unit}-{n_velocity_bins}.json"
+            tokenizer = AwesomeMidiTokenizer.from_file(tokenizer_path)
+
+    model_args = dict(
+        n_layer=cfg.model.n_layer,
+        n_head=cfg.model.n_head,
+        n_embd=cfg.model.n_embd,
+        block_size=dataset_config["sequence_length"],
+        bias=cfg.model.bias,
+        vocab_size=None,
+        dropout=cfg.model.dropout,
+    )
+    checkpoint_model_args = checkpoint["model_args"]
+    for k in ["n_layer", "n_head", "n_embd", "block_size", "bias", "vocab_size"]:
+        model_args[k] = checkpoint_model_args[k]
+
+    gptconf = GPTConfig(**model_args)
+    model = GPT(gptconf)
+    state_dict = checkpoint["model"]
+
+    unwanted_prefix = "_orig_mod."
+    for k, v in list(state_dict.items()):
+        if k.startswith(unwanted_prefix):
+            state_dict[k[len(unwanted_prefix) :]] = state_dict.pop(k)
+
+    model.load_state_dict(state_dict)
+    model.eval()
+    model.to(device)
+
+    return model, tokenizer, cfg
+
+
+def main(args):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, tokenizer, cfg = load_model_and_tokenizer(args.model_path, device)
+    dataset_config = cfg.dataset
+
+    torch.manual_seed(4)
+    torch.cuda.manual_seed(4)
+    torch.backends.cuda.matmul.allow_tf32 = True
+    torch.backends.cudnn.allow_tf32 = True
+
+    ptdtype = {"float32": torch.float32, "bfloat16": torch.bfloat16, "float16": torch.float16}[cfg.system.dtype]
+    ctx = nullcontext() if device == "cpu" else torch.amp.autocast(device_type=device, dtype=ptdtype)
+
+    input_piece = ff.MidiPiece.from_file(args.midi_path)
+    input_notes = input_piece.df
+    input_sequence = tokenizer.encode(input_notes)
+    it = 0
+    generated_noteframes = []
+    total_sequences = len(input_sequence) // dataset_config.sequence_length
+    offset = 0
+    while it + dataset_config.sequence_length < len(input_sequence):
+        sequence = input_sequence[it : it + dataset_config.sequence_length]
+        original_notes = tokenizer.decode(sequence)
+
+        sequence = torch.tensor([sequence], device=device)
+        if it / (dataset_config.sequence_length) % 10 == 0:
+            print(f"generating frame {it // dataset_config.sequence_length} / {total_sequences} ...")
+        with torch.no_grad():
+            with ctx:
+                output = model.generate(
+                    idx=sequence,
+                    max_new_tokens=dataset_config["sequence_length"],
+                    temperature=args.temperature,
+                )
+
+        output: torch.Tensor = output[0].cpu().numpy()
+
+        out_notes = tokenizer.decode(output)
+        out_notes = out_notes[len(original_notes) :]
+
+        out_notes.end -= out_notes.start.min() - offset
+        out_notes.start -= out_notes.start.min() - offset
+
+        offset = out_notes.end.max()
+
+        generated_noteframes.append(out_notes)
+        it += dataset_config.sequence_length
+
+    out_notes = pd.concat(generated_noteframes, ignore_index=True, axis=0).reset_index()
+    out_piece = ff.MidiPiece(out_notes)
+
+    output_midi_path = args.output_path
+    out_piece.to_midi().write(output_midi_path)
+
+    print(f"Generated MIDI saved to {output_midi_path}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate MIDI sequences from a pre-trained model.")
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        required=True,
+        help="Path to the model checkpoint.",
+    )
+    parser.add_argument(
+        "--midi_path",
+        type=str,
+        required=True,
+        help="Path to the input MIDI file.",
+    )
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        required=True,
+        help="Path to save the generated MIDI file.",
+    )
+    parser.add_argument(
+        "--temperature",
+        type=float,
+        required=False,
+        help="Temperature parameter for model.generate",
+        default=1.0,
+    )
+
+    args = parser.parse_args()
+    main(args)
