1e25f13418d0b2916389568f8bee71cc49053a4f
WojciechMat <wmatsad@gmail.com>
2024-07-30 12:03:34 +0200

    return to old generation method

--- gpt2/generation.py
+++ gpt2/generation.py
@@ -239,16 +239,12 @@ def generate_subsequence_iteratively(
         step_target_notes = pd.DataFrame(columns=prompt_notes.columns)
     it = 0
     # Iterate through the piece, generating bass notes in steps
-    target_notes = pd.DataFrame()
     while time + time_step <= end:
         # Calculate the start offset for the bass notes in this step
-        if len(target_notes) > 0:
-            start_offset = target_notes.end.max()
-        else:
-            start_offset = it * time_step
+        start_offset = it * time_step
         it += 1
-        step_prompt_notes.start -= step_prompt_notes.start.min()
-        step_prompt_notes.end -= step_prompt_notes.start.min()
+        step_prompt_notes.start -= start_offset
+        step_prompt_notes.end -= start_offset
 
         step_target_notes = step_target_notes[(step_target_notes.start > 0) & (step_target_notes.end > 0)]
         # Tokenize the current step's prompt and target notes
