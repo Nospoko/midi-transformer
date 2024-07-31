e0302038d4b8dd5b5e074bc8c2110e1283876ae4
WojciechMat <wmatsad@gmail.com>
2024-07-31 10:11:32 +0200

    generation till the last step

--- gpt2/generation.py
+++ gpt2/generation.py
@@ -239,7 +239,7 @@ def generate_subsequence_iteratively(
         step_target_notes = pd.DataFrame(columns=prompt_notes.columns)
     it = 0
     # Iterate through the piece, generating bass notes in steps
-    while time + time_step <= end:
+    while time <= end:
         # Calculate the start offset for the bass notes in this step
         start_offset = it * time_step
         it += 1
