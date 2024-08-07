3987baddd36f9e45e4e15384b9f6dc7ab4b3fd87
WojciechMat <wmatsad@gmail.com>
2024-08-07 17:50:11 +0200

    remove debugging print

--- gpt2/train.py
+++ gpt2/train.py
@@ -433,7 +433,6 @@ def main(cfg: DictConfig):
     total_tokens = 0
     # training loop
     X, Y, mask = get_batch("train")  # fetch the very first batch
-    print(X[:5, :5])
     t0 = time.time()
     local_iter_num = 0  # number of iterations in the lifetime of this process
     raw_model = model.module if ddp else model  # unwrap DDP container if needed
